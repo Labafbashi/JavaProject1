c804490a19e39b04e6e1cafaf68a7a9b5bb1e794
MohammadHossein Labafbashi <92016155+Labafbashi@users.noreply.github.com>
2021-11-19 11:23:41 +0100

    Add comment to code

--- src/SortMain.java
+++ src/SortMain.java
@@ -1,5 +1,6 @@
 public class SortMain {
     public static void main(String[] args) {
+        // Start application with args parse
         if (args.length > 0){
             switch (args[0]) {
                 case "-h":
@@ -14,9 +15,8 @@ public class SortMain {
                     showVersion();
                     break;
                 default:
+                    // Application start from here.
                     if (checkElement(args)) {
-                        System.out.println("Code asli barnameh.");
-
                         int[] argsInt = argsConvertInt(args);
                         Sorter s1 = new Sorter(argsInt);
                         int[] sortedList=s1.toSort();
@@ -30,6 +30,7 @@ public class SortMain {
     }
 
     public static int[] argsConvertInt(String[] args) {
+        // This method convert user value from String to Integer.
         int argsInt[] = new int[args.length];
         for(int i=0;i<args.length;i++){
             argsInt[i]=Integer.parseInt(args[i]);
@@ -38,6 +39,8 @@ public class SortMain {
     }
 
     public static boolean checkElement(String[] args) {
+        // This method start to checking user array list.
+        // If find one element that is not integer, then sent a message for user and wants to try again with only numeric value.
         for(String el : args){
             if(!(isNumeric(el))){
                 System.out.println("value "+el+", that you entered is not an integer.");
@@ -49,10 +52,12 @@ public class SortMain {
     }
 
     public static boolean isNumeric(String str) {
+        // This method with regular expression check that parameter is an integer value or no.
         return str.matches("-?\\d+(\\.\\d+)?");
     }
 
     public static void showVersion() {
+        // This method print out the version of application and some more information.
         System.out.println();
         System.out.println("Java programming course, EC-Utbildning, Goteborg");
         System.out.println("Sort application, version: 1.0");
@@ -60,6 +65,7 @@ public class SortMain {
     }
 
     public static void showHelp() {
+        // This method print out help menu for user.
         System.out.println("Usage: SortMain [-h] [-v] numbers_list");
         System.out.println();
         System.out.println("Description: This program get a list of integer numbers and show you the sorted list.");
--- src/Sorter.java
+++ src/Sorter.java
@@ -22,6 +22,4 @@ public class Sorter {
         }
         return sortedList;
     }
-
-
 }
